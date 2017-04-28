e9297f64b97e237a8e255b267adb273f757c690b
ThanhMary <nguyenthanhmary@yahoo.fr>
2017-04-28 14:33:53 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="fr">
+      <HEAD>
+		<meta charset="utf-8">
+		<TITLE>Page d'accueil</TITLE>
+		<link rel="stylesheet" type="text/css" href="pagestyle.css">
+		
+     </HEAD>
+	<BODY>
+	
+	
+		 <center> <h1>BIENVENUE SUR MA PAGE WEB </h1>
+
+			<h1>Thanh NGUYEN <BR></h1>
+			<h1><A href="http://www.routard.com/guide_voyage_lieu/3055-hanoi.htm"><font color="FBFCFF"> Hanoi</A> <BR></h1>
+			<h1><img src="image/phoco.jpg" width = "500"></h1>
+			<h1>"AVEC DU TEMPS ET DE LA PATIENCE, ON VIENT A BOUT DE TOUT"</h1></center>
+		
+		
+	<center><h2>
+			
+			<A href= "formation.html"> <font color="FBFCFF"> Formation <br></font></A></li>
+			<A href= "loisirs.html"><font color="FBFCFF"> Mes loisirs<br><font></A></li>
+			<A href= "projets.html"><font color="FBFCFF"> Mes projets<br><font></A></li>
+			<A href= "vousetes.html"> <font color="FBFCFF">Contactons-nous!<br><font></A></li>
+		
+	</center></h2><br>
+	<center>QUEL HEURE EST-IL? <br>
+		<?php
+		$aujourdhui = date ("d/m/y");
+		$heure =date("H:i");
+		echo " Nous sommes le $aujourdhui, il est $heure";
+		?> </center>
+	
+	
+	
+	<br><h2><A href= "FormulaireRestau.php"> <font color="FBFCFF"> Vers le Menu restaurant<br><font></A>
+	<A href= "formulaire.html"> <font color="FBFCFF">Vers le formulaire de contact<br><font></A></h2>
+<p>
+	
+</p>	
+<p>
+
+
+<p>
+
+
+	
+
+	
+</body>
+
+</HTML>
